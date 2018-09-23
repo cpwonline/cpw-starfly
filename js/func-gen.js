353ac1b19bb46b975a34069705dda80465e33344
JOSE FELIX RIVAS CARRASCO <juniorrivas185@hotmail.com>
2018-09-23 03:21:08 -0400

    Se han configurado ciertos aspectos de organizacion y legibilidad del codigo, tambien aspesctos del diseño de las noificaciones

--- js/func-gen.js
+++ js/func-gen.js
@@ -1,9 +1,9 @@
 //Variables generales
 	//De estilo
-		estiloContenedor = "background:hsla(0, 0%, 20%, .9);padding:.2cm;margin-bottom:.1cm;overflow:hidden;text-align:right;border-radius:.1cm;-webkit-border-radius:.1cm;-moz-border-radius:.1cm;-o-border-radius:.1cm;transition:.3s all;-webkit-transition:.3s all;-moz-transition:.3s all;-o-transition:.3s all;";
+		estiloContenedor = "background:hsla(0, 0%, 30%, .9);padding:.2cm;margin-bottom:.1cm;overflow:hidden;text-align:right;border-radius:.1cm;-webkit-border-radius:.1cm;-moz-border-radius:.1cm;-o-border-radius:.1cm;transition:.3s all;-webkit-transition:.3s all;-moz-transition:.3s all;-o-transition:.3s all;";
 		estiloTitulo = "font-size:12pt;color:#CCC;margin-bottom:.1cm;";
 		estiloMensaje = "font-size:10pt;color:#FFF;";
-		estiloBoton = "btn-gen";
+		estiloBoton = "btn-gen";//Debes elegir una clase 
 	//De personalización
 		textoBotonGen = "Ok";
 //Fin variables generales
@@ -15,7 +15,7 @@ function starFly(t, m, cierre, duracion){
 		padre = document.getElementById("starFly");
 	//Funciones principales
 		//Función para borrar la notificación del documento
-			this.borrarElemento = function(elemento, nivel){
+			this.borrarElemento_starFly = function(elemento, nivel){
 				switch(nivel){
 					case 0:
 						padre.removeChild(elemento);
@@ -24,16 +24,16 @@ function starFly(t, m, cierre, duracion){
 						padre.removeChild(elemento.parentNode);
 						break;
 				}
-				this.ocultarPanel();
+				this.ocultarPanel_starFly();
 			}
 		//Función para ocultar el panel si no hay notificaciones
-			this.ocultarPanel = function(){
+			this.ocultarPanel_starFly = function(){
 				var c = padre.getElementsByTagName('article');
 				if(c.length < 1)
 					padre.style.display = "none";
 			}
-		this.esperaAuto = function(){
-			borrarElemento(cajaContenedor, 0);
+		this.esperaAuto_starFly = function(){
+			borrarElemento_starFly(cajaContenedor, 0);
 		}
 		
 	//Creamos las cajas principales
@@ -48,7 +48,7 @@ function starFly(t, m, cierre, duracion){
 			cajaMensaje.setAttribute("style", estiloMensaje);
 			cajaBoton.setAttribute("class", estiloBoton);
 		//Añadimos contenido dinámico
-			cajaBoton.setAttribute("onclick", "borrarElemento(this, 1);");
+			cajaBoton.setAttribute("onclick", "borrarElemento_starFly(this, 1);");
 	//Creamos el contenido
 		var textoTitulo = document.createTextNode(t);
 		var textoMensaje = document.createTextNode(m);
@@ -63,14 +63,25 @@ function starFly(t, m, cierre, duracion){
 		if(cierre == 0)
 			cajaContenedor.appendChild(cajaBoton);
 	//Buscamos el elemento posterior
-		var cajaPosterior = padre.querySelector('.pie');
+		var cajaPosterior = padre.getElementById("pie_starFly");
+		alert(cajaPosterior);
 	//Añadimos la notificación a starFly
 		padre.insertBefore(cajaContenedor, cajaPosterior);
 	//Mostramos el panel
 		padre.style.display = "block";
 	//Si es automático el cierre
 		if(cierre == 1)
-			setTimeout(esperaAuto, duracion);
+			setTimeout(esperaAuto_starFly, duracion);
 	//Retornamos la notificación si alguien desea almacenar su dirección en una variable
 		return cajaContenedor;
 }
+
+/*
+	Ejemplo de uso (usando los 3 tipos de notificaciones):
+		var titulo = "Titulo 1";var titulo2 = "Titulo 2";var titulo3 = "Titulo 3";
+		var mensaje = "Mensaje 1";var mensaje2 = "Mensaje 2";var mensaje3 = "Mensaje 3";
+		starFly(titulo, mensaje, 0, 3000);
+		starFly(titulo2, mensaje2, 1, 4000);
+		var ob = starFly(titulo3, mensaje3, 2, 5000);
+		borrarElemento_starFly(ob, 0);
+*/
